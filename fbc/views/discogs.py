bf3ac95dad5fc22ef19d9d4a407424fed3f8a377
Chris Graham <chris.graham@anthology.com>
2026-06-29 23:42:47 -0500

    adding view of discogs releases with no unit

--- fbc/views/discogs.py
+++ fbc/views/discogs.py
@@ -1,5 +1,10 @@
+import logging
 from fbc.util import CustomModelView
 from flask_appbuilder.models.sqla.interface import SQLAInterface
+from flask_appbuilder.models.sqla.filters import (
+    get_field_setup_query,
+)
+from flask_appbuilder.models.filters import BaseFilter
 
 from fbc.models.discogs import (
     Artist,
@@ -14,9 +19,66 @@ from fbc.widgets import (
 from fbc.views.inventory import UnitModelView
 
 
+logger = logging.getLogger()
+
+
+class FilterNotInFunction(BaseFilter):
+    name = "Filter view where field is not in a list returned by a function"
+    arg_name = "not_inf"
+
+    def apply(self, query, func):
+        query, field = get_field_setup_query(query, self.model, self.column_name)
+        return query.filter(~field.in_(func()))
+
+
+def get_unit_releases():
+    from fbc.models.inventory import Unit
+    from fbc import db
+
+    releases = [
+        r.discogs_release_id
+        for r in db.session.query(Unit)
+        .filter(Unit.discogs_release_id.isnot(None))
+        .distinct()
+    ]
+    return releases
+
+
 class DiscogsReleaseModelView(CustomModelView):
     datamodel = SQLAInterface(DiscogsRelease)
-    base_order = ('date_added', 'desc')
+    base_order = ("date_added", "desc")
+    list_widget = DiscogsReleaseListWidget
+    related_views = [UnitModelView]
+    list_columns = [
+        "date_added",
+        "title",
+        "artists",
+        "year",
+        "master_year",
+        "genres",
+        "styles",
+        "purchase_lot",
+        "link_column",
+    ]
+    label_columns = {"link_column": "Links"}
+    edit_columns = [
+        "title",
+        "artists",
+        "year",
+        "master_id",
+        "master_year",
+        "genres",
+        "styles",
+        "discogs_instances",
+        "released",
+        "folder",
+    ]
+
+
+class DiscogsReleaseModelNoUnitView(CustomModelView):
+    datamodel = SQLAInterface(DiscogsRelease)
+    base_filters = [["release_id", FilterNotInFunction, get_unit_releases]]
+    base_order = ("date_added", "desc")
     list_widget = DiscogsReleaseListWidget
     related_views = [UnitModelView]
     list_columns = [
